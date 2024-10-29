fdad6da1a5781668b25c1a90292213cdb49f5c75
AlfitoLeonard159 <alfitoleonard59@sma.belajar.id>
2024-10-29 11:14:54 +0800

    Add files via upload

---
+++ fito.py
@@ -0,0 +1,14 @@
+a = [2, 4, 5, 21, 7, 5, 2, 2, 5]
+
+x = 2
+
+jumlah_kemunculan = 0
+indeks = []
+
+for i in range(len(a)):
+    if a[i] == x:
+        jumlah_kemunculan += 1
+        indeks.append(i)
+
+# Menampilkan hasi
+print(f"Variabel x = {jumlah_kemunculan} indeks {', '.join(map(str, indeks))}")
